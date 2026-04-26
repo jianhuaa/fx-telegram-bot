a91c07bb59cf2c612b188a8749a8050620f6a844
jianhuaa <65246283+jianhuaa@users.noreply.github.com>
2026-04-26 21:22:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1431,7 +1431,7 @@ def show_global_birdseye(df_inds, df_all_ret):
                     # PROFIT
                     s_blk(g('Gross Marg %_Score')), s_blk(g('Op Marg %_Score')), s_blk(g('Net Marg %_Score')),             
                     # FLOWS
-                    s_blk(g('Op CF (M)_Score')), s_blk(g('FCF (M)_Score')), s_bin(g('Inv CF (M)_Score')), '⬛', s_bin(g('Self-Funding_Score')), 
+                    s_blk(g('Op CF (M)_Score')), s_blk(g('FCF (M)_Score')), s_bin(g('Inv CF (M)_Score')), s_blk(g('Fin CF (M)_Score')), s_bin(g('Self-Funding_Score')), 
                     # DEBT
                     s_blk(g('Cash & STI (M)_Score')), s_blk(g('ST Debt (M)_Score')), s_blk(g('LT Debt (M)_Score')), s_blk(g('Cash/Debt Ratio_Score')), s_blk(g('Goodwill, Net (M)_Score')), 
                     # OPT (Placeholders for now)
